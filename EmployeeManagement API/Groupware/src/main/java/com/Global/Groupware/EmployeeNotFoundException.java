de7a9296c0f0deea811afeb13a3d45d73313cfe2
Geethamadhuri16 <84759801+Geethamadhuri16@users.noreply.github.com>
2023-08-09 16:32:10 +0530

    Delete EmployeeManagement API/Groupware directory

--- EmployeeManagement API/Groupware/src/main/java/com/Global/Groupware/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.Global.Groupware;
-
-public class EmployeeNotFoundException extends RuntimeException {
-	public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-
-
-}
